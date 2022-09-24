c1683d8f08b56a6336ef25af9e8a7d89df61f498
Abdullah Olcum <abdullaholcum123456@gmail.com>
2022-09-24 23:47:44 +0300

    added more work2

--- src/main/java/work1/Hello.java
+++ src/main/java/work1/Hello.java
@@ -5,7 +5,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Work1 started");
         System.out.println("Created git repository");
-
+        System.out.println("Sharing project on github.com");
+        System.out.println("continue commiting");
     }
 
 
